7db556a9b3dbd178a5b8eab550df9d2d27beabdf
Aniketshah3767 <79820398+Aniketshah3767@users.noreply.github.com>
2024-07-20 09:58:28 +0545

    Time: 55 ms (54.15%), Space: 38.2 MB (19.11%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -3,13 +3,12 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
-        queue<pair<pair<int,int>,int>>q;
-        //first for storing coordinates and another for stroing distance.
-        vector<vector<int>>vis(n,vector<int>(m,0));
-        //for stoting answer
-        vector<vector<int>>ans(n,vector<int>(m,0));
 
-        for(int i = 0 ;  i < n ;i++){
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        vector<vector<int>> dist(n, vector<int>(m, 0));
+        queue<pair<pair<int,int>,int>>q;
+
+        for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < m ;j++){
                 if(mat[i][j] == 0){
                     q.push({{i,j},0});
@@ -24,21 +23,19 @@ public:
         while(!q.empty()){
             int row = q.front().first.first;
             int col = q.front().first.second;
-            int dist = q.front().second;
-
+            int steps = q.front().second;
             q.pop();
-            
-            ans[row][col] = dist;
-
-            for(int i = 0 ; i < 4 ;i++){
+            dist[row][col] = steps;
+           
+            for(int i = 0 ; i < 4 ; i++){
                 int nrow = row + delrow[i];
                 int ncol = col + delcol[i];
-                if(nrow < n && ncol < m && ncol >= 0 && nrow >= 0 && vis[nrow][ncol] == 0){
+                if(nrow >= 0 && ncol >= 0 && nrow < n && ncol < m && !vis[nrow][ncol]){
                     vis[nrow][ncol] = 1;
-                    q.push({{nrow,ncol},dist+1});
+                    q.push({{nrow,ncol},steps+1});
                 }
             }
         }
-        return ans;
+        return dist;
     }
 };
