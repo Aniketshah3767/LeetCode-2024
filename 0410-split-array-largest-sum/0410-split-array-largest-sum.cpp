f44f8df957b118a63b0b28f7b6bb6b4eaafea46a
Aniketshah3767 <79820398+Aniketshah3767@users.noreply.github.com>
2024-07-22 22:39:25 +0545

    Time: 4 ms (41.27%), Space: 10 MB (5.76%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,57 +1,16 @@
-// class Solution {
-
-// private:
-//     int func(vector<int> nums, int number) {
-//         int n = nums.size();
-//         int  count= 1;
-//         int currsum = 0;
-
-//         for (int i = 0; i < n; i++) {
-//             if (currsum + nums[i] <= number) {
-//                 currsum += nums[i];
-//             } else {
-//                  count++;
-//                 currsum = nums[i];
-//             }
-//         }
-
-//         return count;
-//     }
-
-// public:
-//     int splitArray(vector<int>& nums, int k) {
-
-//         if (nums.size() < k)
-//             return -1;
-
-//         int n = nums.size();
-//         int high = accumulate(nums.begin(), nums.end(), 0);
-//         int low = *max_element(nums.begin(), nums.end());
-
-//         while (high <= low) {
-//             int subarray = func(nums, k);
-//             int mid = low + (high - low) / 2;
-//             if (subarray > k) {
-//                 low = mid + 1;
-//             } else {
-//                 high = mid - 1;
-//             }
-//         }
-//         return low;
-//     }
-// };
 class Solution {
+
 private:
-    int func(vector<int>& nums, int number) {
+    int func(vector<int> nums, int number) {
         int n = nums.size();
-        int count = 1;
+        int  count= 1;
         int currsum = 0;
 
         for (int i = 0; i < n; i++) {
             if (currsum + nums[i] <= number) {
                 currsum += nums[i];
             } else {
-                count++;
+                 count++;
                 currsum = nums[i];
             }
         }
@@ -61,6 +20,7 @@ private:
 
 public:
     int splitArray(vector<int>& nums, int k) {
+
         if (nums.size() < k)
             return -1;
 
@@ -68,7 +28,7 @@ public:
         int high = accumulate(nums.begin(), nums.end(), 0);
         int low = *max_element(nums.begin(), nums.end());
 
-        while (low <= high) {
+        while (low<=high) {
             int mid = low + (high - low) / 2;
             int subarray = func(nums, mid);
             if (subarray > k) {
@@ -80,3 +40,43 @@ public:
         return low;
     }
 };
+// class Solution {
+// private:
+//     int func(vector<int>& nums, int number) {
+//         int n = nums.size();
+//         int count = 1;
+//         int currsum = 0;
+
+//         for (int i = 0; i < n; i++) {
+//             if (currsum + nums[i] <= number) {
+//                 currsum += nums[i];
+//             } else {
+//                 count++;
+//                 currsum = nums[i];
+//             }
+//         }
+
+//         return count;
+//     }
+
+// public:
+//     int splitArray(vector<int>& nums, int k) {
+//         if (nums.size() < k)
+//             return -1;
+
+//         int n = nums.size();
+//         int high = accumulate(nums.begin(), nums.end(), 0);
+//         int low = *max_element(nums.begin(), nums.end());
+
+//         while (low <= high) {
+//             int mid = low + (high - low) / 2;
+//             int subarray = func(nums, mid);
+//             if (subarray > k) {
+//                 low = mid + 1;
+//             } else {
+//                 high = mid - 1;
+//             }
+//         }
+//         return low;
+//     }
+// };
