02362a77d745ccc736343e9af1c41ad526098a70
Aniketshah3767 <79820398+Aniketshah3767@users.noreply.github.com>
2024-08-13 23:37:51 +0530

    Time: 6 ms (14.46%), Space: 13.1 MB (6.99%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<char>st;
+        for(int i = 0 ; i < s.size() ; i++){
+            if(s[i] != ']'){
+                st.push(s[i]);
+            }
+            else{
+                string curr = "";
+
+                while(st.top() != '['){
+                    curr = st.top() + curr;
+                    st.pop();
+                }
+
+                st.pop();
+
+                string number = "";
+
+                while(!st.empty() && isdigit(st.top())){
+                    number = st.top() + number;
+                    st.pop();
+                }
+
+                int k_time = stoi(number);
+                while(k_time--){
+                    for(int p = 0 ; p < curr.size() ; p++){
+                        st.push(curr[p]);
+                    }
+                }
+            }
+        }
+        s = "";
+        while(!st.empty()){
+            s = st.top() + s;
+            st.pop();
+        }
+        return s;
+        
+    }
+};
