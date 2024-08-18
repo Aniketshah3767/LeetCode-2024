46559eb5e046e12c3fe7cdd45ec552938558ee2f
Aniketshah3767 <79820398+Aniketshah3767@users.noreply.github.com>
2024-08-18 14:38:50 +0530

    Time: 14 ms (26.78%), Space: 10.5 MB (26.36%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -1,28 +1,55 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        deque<char> st;
-        int keep = num.size() - k;
+        // deque<char> st;
+        // int keep = num.size() - k;
 
-        for (char digit : num) {
-            while (!st.empty() && st.back() > digit && k > 0) {
-                st.pop_back();
+        // for (char digit : num) {
+        //     while (!st.empty() && st.back() > digit && k > 0) {
+        //         st.pop_back();
+        //         k--;
+        //     }
+        //     st.push_back(digit);
+        // }
+        // while (k > 0) {
+        //     st.pop_back();
+        //     k--;
+        // }
+
+        // while (!st.empty() && st.front() == '0') {
+        //     st.pop_front();
+        // }
+
+        // string ans;
+        // copy(st.begin(), st.end(), back_inserter(ans));
+
+        // return ans.empty() ? "0" : ans;
+        stack<char>st;
+        for(auto it: num){
+            while(!st.empty() && k && it < st.top()){
+                st.pop();
                 k--;
             }
-            st.push_back(digit);
+            st.push(it);
         }
-        while (k > 0) {
-            st.pop_back();
+
+        string ans = "";
+        while(!st.empty() && k){
+            st.pop();
             k--;
         }
-
-        while (!st.empty() && st.front() == '0') {
-            st.pop_front();
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
         }
-
-        string ans;
-        copy(st.begin(), st.end(), back_inserter(ans));
-
-        return ans.empty() ? "0" : ans;
+        reverse(ans.begin(), ans.end());
+        int i = 0;
+        int n = ans.length();
+        while(i < n && ans[i] == '0'){
+            i++;
+        }
+        if(i == n)
+            return "0";
+        return ans.substr(i);
     }
 };
