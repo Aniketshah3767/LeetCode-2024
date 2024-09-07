4643b68a6ecbc3524e3c83d5b7ce364c0a849bec
Aniketshah3767 <79820398+Aniketshah3767@users.noreply.github.com>
2024-09-07 18:05:37 +0530

    Time: 1035 ms (44.31%), Space: 0B (100%) - LeetHub

--- 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
@@ -1,4 +1,7 @@
 # Write your MySQL query statement below
+# Write a solution to find the percentage of the users registered in each contest rounded to two decimals.
+# Return the result table ordered by percentage in descending order. In case of a tie, order it by contest_id in ascending order.
+
 select r.contest_id, round(count(r.user_id) * 100.0 / (select count(user_id) from Users),2) as percentage
 from register r
 group by r.contest_id
