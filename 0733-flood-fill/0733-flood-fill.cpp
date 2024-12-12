11a31e89a37bcf3101838fe418aa7f8b33d21385
Aniketshah3767 <79820398+Aniketshah3767@users.noreply.github.com>
2024-12-12 16:46:36 +0530

    Time: 0 ms (100%), Space: 18.2 MB (12.09%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,29 +1,63 @@
 class Solution {
 private:
-    void dfs(int row, int col, vector<vector<int>>& image, vector<vector<int>>& ans, int color, int inicolor, int delrow[], int delcol[]) {
-        int n = image.size();
-        int m = image[0].size();
-        ans[row][col] = color;
+//     void dfs(int row, int col, vector<vector<int>>& image, vector<vector<int>>& ans, int color, int inicolor, int delrow[], int delcol[]) {
+//         int n = image.size();
+//         int m = image[0].size();
+//         ans[row][col] = color;
         
-        for (int i = 0; i < 4; i++) {
-            int nrow = row + delrow[i];
-            int ncol = col + delcol[i];
-            if (nrow < n && ncol < m && ncol >= 0 && nrow >= 0 &&
-                image[nrow][ncol] == inicolor && ans[nrow][ncol] != color ) {
-                dfs(nrow, ncol, image, ans, color, inicolor,delrow, delcol);
-            }
-        }
-    }
+//         for (int i = 0; i < 4; i++) {
+//             int nrow = row + delrow[i];
+//             int ncol = col + delcol[i];
+//             if (nrow < n && ncol < m && ncol >= 0 && nrow >= 0 &&
+//                 image[nrow][ncol] == inicolor && ans[nrow][ncol] != color ) {
+//                 dfs(nrow, ncol, image, ans, color, inicolor,delrow, delcol);
+//             }
+//         }
+//     }
+
+// public:
+//     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc,int color) {
+//         vector<vector<int>> ans = image;
+//         int inicolor = image[sr][sc];
+//         int delrow[] = {-1, 0, 1, 0};
+//         int delcol[] = {0 ,1, 0, -1};
+//         dfs(sr, sc, image, ans, color, inicolor, delrow, delcol);
+
+//         return ans;
+//     }
+// };
+
+
 
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc,
-                                  int color) {
-        vector<vector<int>> ans = image;
-        int inicolor = image[sr][sc];
-        int delrow[] = {-1, 0, 1, 0};
-        int delcol[] = {0 ,1, 0, -1};
-        dfs(sr, sc, image, ans, color, inicolor, delrow, delcol);
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc,int color) {
+        int curr = image[sr][sc];
+        if(curr == color)  return image;
 
-        return ans;
+        int n = image.size();
+        int m = image[0].size();
+
+        queue<pair<int,int>>q;
+        q.push({sr,sc});
+        image[sr][sc]= color;
+
+        vector<pair<int,int>>dir = {{-1,0},{0,1},{1,0},{0,-1}};
+
+        while(!q.empty()){
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+
+            for(auto [dr,dc]: dir){
+                int newr = row + dr;
+                int newc = col + dc;
+
+                if(newr >= 0 && newr < n && newc >= 0 && newc < m && image[newr][newc] == curr){
+                    q.push({newr,newc});
+                    image[newr][newc] = color;
+                }
+            }
+        }
+        return image;
     }
 };
