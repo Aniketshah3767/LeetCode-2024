e46032e9e93be83e50525b22e5769b5408293f1d
Aniketshah3767 <79820398+Aniketshah3767@users.noreply.github.com>
2025-05-31 00:54:50 +0530

    Time: 586 ms (42.27%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,10 +1,35 @@
 # Write your MySQL query statement below
-select distinct(log1.num) as ConsecutiveNums from 
-    logs log1, 
-    logs log4,
-    logs log3
-where 
-    log1.id = log4.id - 1 AND
-    log4.id = log3.id - 1 AND
-    log1.num = log4.num AND
-    log4.num = log3.num
+
+-- Solutiuon 1
+-- select distinct(log1.num) as ConsecutiveNums from 
+--     logs log1, 
+--     logs log4,
+--     logs log3
+-- where 
+--     log1.id = log4.id - 1 AND
+--     log4.id = log3.id - 1 AND
+--     log1.num = log4.num AND
+--     log4.num = log3.num
+
+
+-- Solution 2
+-- select distinct num as ConsecutiveNums 
+-- from (
+--     select num, 
+--     lead(num) over (order by id) as after,
+--     lag(num) over (order by id) as b4
+-- from logs
+-- )
+-- where
+--     num = after AND num = b4
+
+
+SELECT DISTINCT num AS ConsecutiveNums
+FROM (
+    SELECT num, 
+           LEAD(num) OVER (ORDER BY id) AS after,
+           LAG(num) OVER (ORDER BY id) AS b4
+    FROM Logs
+) AS sub
+WHERE num = after AND num = b4;
+
