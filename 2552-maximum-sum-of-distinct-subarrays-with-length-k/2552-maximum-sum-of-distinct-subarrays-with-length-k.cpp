cc9cecea0fce8d25664ba39614f5fc73fd4e7333
Aniketshah3767 <79820398+Aniketshah3767@users.noreply.github.com>
2025-06-17 23:14:36 +0530

    Time: 95 ms (83.68%), Space: 95.3 MB (75.9%) - LeetHub

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,27 +1,54 @@
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
+        //-------------------brute Force approach------------------
+    //     long long maxi = 0;
+    //     int n = nums.size();
+
+    //     for(int i = 0 ; i < n ; i++){
+    //         unordered_set<int>set;
+    //         long long sum = 0;
+
+    //         for(int j = i ; j < i + k && j < n ;j++){
+    //             if(set.find(nums[j]) != set.end()){
+    //                 break;
+    //             }
+    //             set.insert(nums[j]);
+    //             sum+=nums[j];
+    //         }
+
+    //         if(set.size() == k)
+    //             maxi = max(sum,maxi);
+    //     }
+
+
+    // return maxi;
+
+    // -------------------------- Optimal Approach------------------------
+
+    long long max_sum = 0;
+        long long window_sum = 0;
+        int start = 0;
         int n = nums.size();
-        unordered_map<int,int>mpp;
-        long long start = 0; 
-        long long count = 0, ans = 0;
-        for(int i = 0 ; i < n ; i++){
-            count += nums[i];
-            if(mpp.find(nums[i]) != mpp.end()){
-                while(start <= mpp[nums[i]]){
-                    count -= nums[start];
-                    start++;
+        std::unordered_map<int, int> map;
+
+        for (int i = 0; i < n; ++i) {
+            window_sum += nums[i];
+            map[nums[i]]++;
+
+            if (i - start + 1 == k) {
+                if (map.size() == k) {
+                    max_sum = std::max(max_sum, window_sum);
                 }
-                mpp[nums[i]] = i;
-            }
-            mpp[nums[i]] = i;
-            if(i-start + 1 == k){
-                ans=max(ans,count);
-                count -= nums[start];
-                mpp.erase(nums[start]);
-                start++;
+
+                window_sum -= nums[start];
+                if (--map[nums[start]] == 0) {
+                    map.erase(nums[start]);
+                }
+                ++start;
             }
         }
-    return ans;
+
+        return max_sum;
     }
 };
