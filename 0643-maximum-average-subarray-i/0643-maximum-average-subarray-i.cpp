792d508750b94195c2e32da9f4b86137dbf27094
Aniketshah3767 <79820398+Aniketshah3767@users.noreply.github.com>
2025-06-17 17:51:16 +0530

    Time: 5 ms (13.72%), Space: 113.7 MB (44.66%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -5,16 +5,30 @@ public:
         double size = 0;
         double i = 0; 
         double j = 0;
+        // while(j<k){
+        //     sum += nums[j];
+        //     j++;
+        // }
+
+        // double total = sum / k;
+        // while(j<nums.size()){
+        //     sum += nums[j] - nums[i];
+        //     total = max(total , sum / k);
+        //     i++; j++;
+        // }
+        // return total;
+
         while(j<k){
             sum += nums[j];
             j++;
         }
 
+
         double total = sum / k;
         while(j<nums.size()){
             sum += nums[j] - nums[i];
-            total = max(total , sum / k);
-            i++; j++;
+            total = max(total,sum/k);
+            i++;j++;
         }
         return total;
     }
