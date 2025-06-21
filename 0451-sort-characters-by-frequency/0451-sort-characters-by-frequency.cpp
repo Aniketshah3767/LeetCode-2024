f4d63ebcf0fc050da81a970121a02428dc3f6fe2
Aniketshah3767 <79820398+Aniketshah3767@users.noreply.github.com>
2025-06-21 23:57:32 +0530

    Time: 0 ms (100%), Space: 11.3 MB (42.54%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -7,9 +7,9 @@ public:
             mpp[it]++;
         }
 
-        priority_queue<pair<int,int>>pq;
-        for(auto it : mpp){
-            pq.push({it.second, it.first});
+        priority_queue<pair<int,char>>pq;
+        for(auto it:mpp){
+            pq.push({it.second,it.first});
         }
 
         while(!pq.empty()){
@@ -19,5 +19,25 @@ public:
             ans.append(count,c);
         }
         return ans;
+
+
+        // string ans = "";
+        // unordered_map<char,int>mpp;
+        // for(auto it : s){
+        //     mpp[it]++;
+        // }
+
+        // priority_queue<pair<int,int>>pq;
+        // for(auto it : mpp){
+        //     pq.push({it.second, it.first});
+        // }
+
+        // while(!pq.empty()){
+        //     char c = pq.top().second;
+        //     int count = pq.top().first;
+        //     pq.pop();
+        //     ans.append(count,c);
+        // }
+        // return ans;
     }
 };
