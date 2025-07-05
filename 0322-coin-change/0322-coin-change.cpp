f89c4e75f47147e8bce0fdbb88e7639f27967c90
Aniketshah3767 <79820398+Aniketshah3767@users.noreply.github.com>
2025-07-05 17:45:02 +0530

    Time: 51 ms (25.65%), Space: 49.3 MB (29.44%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,17 +1,38 @@
 class Solution {
+private:
+    int func(int ind, int amount, vector<int>& coins,vector<vector<int>>&dp) {
+        if (ind == 0) {
+            if (amount % coins[0] == 0)
+                return amount / coins[0];
+            return 1e9;
+        }
+
+        if(dp[ind][amount] != -1){
+            return dp[ind][amount];
+        }
+
+        int nottake = 0 + func(ind - 1, amount, coins,dp);
+        int take = INT_MAX;
+        if (coins[ind] <= amount) {
+            take = 1 + func(ind, amount - coins[ind], coins,dp);
+        }
+
+        return dp[ind][amount] = min(take, nottake);
+    }
+
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int>dp(amount+1,amount+1);
-        dp[0] = 0;
-        for(int i = 1 ; i < amount+1 ; i++){
-            for(int j : coins){
-                if(i - j  >= 0)
-                    dp[i] = min(dp[i] , 1 + dp[i-j]);
-            }
-        }
-        if(dp[amount] == amount+1)
-            return -1;
+        // vector<int>ans;
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
 
-        return dp[amount];
+
+        int temp = func(coins.size() - 1, amount, coins,dp);
+
+        if (temp == 1e9) {
+            return -1;
+        } else {
+            return temp;
+        }
     }
 };
