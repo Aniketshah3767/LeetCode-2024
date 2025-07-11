f90f00fe61b66efb45dbe02ac17910b2d31a9cad
Aniketshah3767 <79820398+Aniketshah3767@users.noreply.github.com>
2025-07-11 11:48:19 +0530

    Time: 0 ms (100%), Space: 17.9 MB (21.1%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,23 +1,36 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int max_prod = 1;
+        // int max_prod = 1;
 
+        // int maxi = INT_MIN;
+        // for (int i = 0; i < nums.size(); i++) {
+        //     max_prod *= nums[i];
+        //     maxi = max(max_prod, maxi);
+        //     if (max_prod == 0) {
+        //         max_prod = 1;
+        //     }
+        // }
+        // max_prod = 1;
+
+        // for(int i = nums.size()- 1 ;i>=0;i--){
+        //     max_prod *= nums[i];
+        //     maxi = max(maxi,max_prod);
+        //     if(max_prod == 0)
+        //         max_prod = 1;
+        // }
+        // return maxi;
+        int prefix = 1;
+        int suffix = 1;
         int maxi = INT_MIN;
-        for (int i = 0; i < nums.size(); i++) {
-            max_prod *= nums[i];
-            maxi = max(max_prod, maxi);
-            if (max_prod == 0) {
-                max_prod = 1;
-            }
-        }
-        max_prod = 1;
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(prefix == 0) prefix = 1;
+            if(suffix == 0) suffix = 1;
 
-        for(int i = nums.size()- 1 ;i>=0;i--){
-            max_prod *= nums[i];
-            maxi = max(maxi,max_prod);
-            if(max_prod == 0)
-                max_prod = 1;
+            prefix = prefix * nums[i];
+            suffix = suffix * nums[nums.size()-i-1];
+
+            maxi = max(maxi,max(prefix,suffix));
         }
         return maxi;
     }
