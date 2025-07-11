1810945f8370ea817eb69544ed3a075a128bbf0d
Aniketshah3767 <79820398+Aniketshah3767@users.noreply.github.com>
2025-07-11 23:33:15 +0530

    Time: 0 ms (100%), Space: 9.6 MB (79.36%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
@@ -1,9 +1,22 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-        if(str1 + str2 != str2+str1)
+        // if(str1 + str2 != str2+str1)
+        //     return "";
+        // return str1.substr(0,gcd(str1.size(),str2.size())); 
+        if (str1 + str2 != str2 + str1) {
             return "";
+        }
         
-        return str1.substr(0,gcd(str1.size(),str2.size())); 
+        int a = str1.size();
+        int b = str2.size();
+        
+        while (b > 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        
+        return str1.substr(0, a);
     }
 };
