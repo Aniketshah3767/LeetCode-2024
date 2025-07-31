73c120d4d947807273c50ddf7b4dcfbab571813d
Aniketshah3767 <79820398+Aniketshah3767@users.noreply.github.com>
2025-07-31 21:47:35 +0530

    Time: 15 ms (72.18%), Space: 25 MB (76.44%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,7 +3,7 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> mpp;
 
-        for (auto& s : strs) {
+        for (auto s : strs) {
             string temp = s;
             sort(temp.begin(), temp.end());  
             mpp[temp].push_back(s);         
